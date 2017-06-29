39fd5cb959b77aae04b79ceb0e41cbac82f503b5
Hack Reactor Student <student@hackreactor.com>
2017-06-29 16:28:24 -0700

    Passed all test except for collison handling

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -6,15 +6,26 @@ var HashTable = function() {
 };
 
 HashTable.prototype.insert = function(k, v) {
+  
+  if (this.retrieve(k)) {   
+    this._limit *= 2;
+    var resizeLimitedArray = {};
+    this._limit.each(function(value, key, storage) {
+      resizeLimitedArray[key] = value;
+    });
+  }
+
   var index = getIndexBelowMaxForKey(k, this._limit);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage.get(index);
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(index, undefined);
 };
 
 
